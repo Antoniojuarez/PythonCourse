fe8fdbf7c02c2e58faace8649003026291265067
Antoniomjg <antoniomjg1@gmail.com>
2020-01-20 19:33:05 +0100

    Sintaxis condicionales if else en List Comprehension

--- FirstCode/listComprehensions.py
+++ FirstCode/listComprehensions.py
@@ -2,10 +2,16 @@ temps = [221, 234, 340, 230]
 
 new_temps = [temp / 10 for temp in temps]
 
-temps2 = [221, 234, 340, -9999, 230]
-
-new_temps2 = [temp / 10 for temp in temps if temp != -9999]
-
 print(new_temps)
 
+temps2 = [221, 234, 340, -9999, 230]
+
+new_temps2 = [temp / 10 for temp in temps2 if temp != -9999]
+
 print(new_temps2)
+
+temps3 = [221, 234, 340, -9999, 230]
+
+new_temps3 = [temp / 10 if temp != -9999 else 0 for temp in temps3]
+
+print(new_temps3)
