ede5e7e4b33ac015651e41a690cdb3be448e6e00
Antoniomjg <antoniomjg1@gmail.com>
2020-01-22 20:52:58 +0100

    Uso de librerias externas

--- FirstCode/modulos.py
+++ FirstCode/modulos.py
@@ -1,6 +1,22 @@
 import time
+import os
+import pandas
+
+
+real = 0
+
+while real:
+    if os.path.exists("vegetables.txt"):
+        with open("vegetables.txt") as file:
+            print(file.read())
+    else:
+        print("File don't exist.")
+    time.sleep(10)
 
 while True:
-    with open("vegetables.txt") as file:
-        print(file.read())
-        time.sleep(5)
+    if os.path.exists("temps_today.csv"):
+        data = pandas.read_csv("temps_today.csv")
+        print(data.mean()["st1"])
+    else:
+        print("File dont exist.")
+    time.sleep(10)
