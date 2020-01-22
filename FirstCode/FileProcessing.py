cc062520bdb8740ac26a0bc9ddd034160273e3da
Antoniomjg <antoniomjg1@gmail.com>
2020-01-22 19:46:40 +0100

    Abrir fichero con sintaxis with

--- FirstCode/FileProcessing.py
+++ FirstCode/FileProcessing.py
@@ -1,4 +1,9 @@
 myfile = open("fruits.txt")
 content = myfile.read()
 myfile.close()
-print(myfile.read())
+print(content)
+
+with open("fruits.txt") as myfile:
+    content = myfile.read()
+
+print(content)
